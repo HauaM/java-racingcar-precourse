6340425fab6f89148459dec784ed5380f24c7873
HauaM <ahwo22@gmail.com>
2021-10-11 15:56:06 +0900

    test:랜덤숫자 배열을 입력받아 자동차의 최종상태를 테스트함.

--- src/test/java/racinggame/ApplicationTest.java
+++ src/test/java/racinggame/ApplicationTest.java
@@ -1,15 +1,11 @@
 package racinggame;
 
-import model.Car;
-import model.NameCerfStatus;
-import model.UserInputCarNames;
+import model.*;
 import nextstep.test.NSTest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 public class ApplicationTest extends NSTest {
     private static final int MOVING_FORWARD = 4;
@@ -39,15 +35,15 @@ public class ApplicationTest extends NSTest {
     }
 
     /**
-     *  경계값 테스트
-     *  입력 6 - > 검증 false
-     *  입력 5 - > 검증 true
-     *  입력 0 - > 검증 false
-     *  입력 1 - > 검증 true
+     * 경계값 테스트
+     * 입력 6 - > 검증 false
+     * 입력 5 - > 검증 true
+     * 입력 0 - > 검증 false
+     * 입력 1 - > 검증 true
      */
     @Test
     @DisplayName("자동자 이름 5글자 확인")
-    void 자동차이름5글자확인(){
+    void 자동차이름5글자확인() {
         assertThat(Car.validNameLength("123456")).isFalse();
         assertThat(Car.validNameLength("12345")).isTrue();
         assertThat(Car.validNameLength("")).isFalse();
@@ -55,29 +51,56 @@ public class ApplicationTest extends NSTest {
     }
 
     /**
-     *  경계값 테스트
-     *  입력 [a, bbbbbb ] -> 검증 true
+     * 경계값 테스트
+     * 입력 [a, bbbbbb ] -> 검증 true
      */
     @Test
     @DisplayName("모든 이름들이 규칙에 맞게 들어왔는지 확인")
-    void 자동차이름검증기능확인(){
+    void 자동차이름검증기능확인() {
         UserInputCarNames userInputCarNames = new UserInputCarNames();
 
-        userInputCarNames.validationNameLength((new String[]{"a","bbbbb"}));
+        userInputCarNames.validationNameLength((new String[]{"a", "bbbbb"}));
         assertThat(userInputCarNames.chkStringIsErr()).isEqualTo(NameCerfStatus.NORMER);
     }
 
     @Test
     @DisplayName("이름 잘못 입력했을 때 예외 처리나오는지에 대한 검증")
     void 이름_예외처리_테스트() {
-
         UserInputCarNames userInputCarNames = new UserInputCarNames();
 
         assertThatThrownBy(() -> {
             userInputCarNames.validationNameLength((new String[]{"aaaaaa"}));
         }).isInstanceOf(Exception.class)
-                .hasMessageContaining ("[ERROR]");
-    };
+                .hasMessageContaining("[ERROR]");
+    }
+
+    ;
+
+    @Test
+    void 최종자동차상테확인_0() {
+        int[] randomValues = new int[]{1, 1, 3, 3, 3};
+        int count = 0;
+        for (int i = 0; i < randomValues.length; i++) {
+            new StatusGoAndStop(randomValues[i]);
+            if (StatusGoAndStop.getStatus().equals(StatusEnum.GO)) {
+                count++;
+            }
+        }
+        assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    void 최종자동차상테확인_4() {
+        int[] randomValues = new int[]{1, 2, 3, 4, 5, 9, 9};
+        int count = 0;
+        for (int i = 0; i < randomValues.length; i++) {
+            new StatusGoAndStop(randomValues[i]);
+            if (StatusGoAndStop.getStatus().equals(StatusEnum.GO)) {
+                count++;
+            }
+        }
+        assertThat(count).isEqualTo(4);
+    }
 
     @AfterEach
     void tearDown() {
